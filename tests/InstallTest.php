7cf0fc9eeb0aaaa68c880dcb9543666ea3d9f332
Steve Grunwell <stevegrunwell@gmail.com>
2018-06-27 13:35:04 -0400

    Instead of counting on PHPUnit to generate the mods-available directories, let Travis handle it

--- tests/InstallTest.php
+++ tests/InstallTest.php
@@ -9,43 +9,14 @@ use PHPUnit\Framework\TestCase;
 
 class InstallTest extends TestCase
 {
-    private static $createdDirectories = [];
-
-    /**
-     * Get an array of all PHP versions we want to use for testing configuration.
-     *
-     * @return array
-     */
-    public static function getTestedVersions(): array
-    {
-        return [
-            '7.0',
-            '7.1',
-            '7.2',
-            '50.0', // Something very unlikely to exist.
-        ];
-    }
-
-    /**
-     * If the local filesystem doesn't have them, create mods-available directories.
-     *
-     * @beforeClass
-     */
-    public static function createModsAvailable()
-    {
-        foreach (self::getTestedVersions() as $version) {
-            if (mkdir('/etc/php/' . $version . '/mods-available', 0777, true)) {
-                self::$createdDirectories[] = '/etc/php/' . $version;
-            }
-        }
-    }
-
     public function testInstallsRunkit()
     {
         $this->assertFalse(
             extension_loaded('runkit'),
             'Expected the Runkit extension to not yet be loaded.'
         );
+
+        $version = PHP_MAJOR_VERSION . '.' . PHP_MINOR_VERSION;
 
         exec(dirname(__DIR__) . '/bin/install-runkit.sh', $output, $exitCode);
 
@@ -54,13 +25,10 @@ class InstallTest extends TestCase
             (bool) preg_match('/^runkit\s/m', shell_exec('pecl list')),
             'The Runkit extension should have been loaded.'
         );
-
-        foreach (self::getTestedVersions() as $version) {
-            $this->assertTrue(
-                file_exists('/etc/php/' . $version . '/mods-available/runkit.ini'),
-                'Expected a runkit.ini file to be created for PHP ' . $version
-            );
-        }
+        $this->assertTrue(
+            file_exists('/etc/php/' . $version . '/mods-available/runkit.ini'),
+            'Expected a runkit.ini file to be created for PHP ' . $version . '.'
+        );
     }
 
     /**
@@ -73,19 +41,6 @@ class InstallTest extends TestCase
         $this->assertTrue(function_exists('runkit_constant_remove'));
         $this->assertTrue(function_exists('runkit_function_add'));
         $this->assertTrue(function_exists('runkit_function_remove'));
-    }
-
-    /**
-     * Remove directories that were only created for tests.
-     *
-     * @afterClass
-     */
-    public static function removeModsAvailable()
-    {
-        foreach (self::$createdDirectories as $dir) {
-            unlink($dir . '/runkit.ini');
-            rmdir($dir);
-        }
     }
 
     /**
